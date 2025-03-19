85d4406a4a5f7e89c8205a343d553366bf01f851
Akitano2025 <aneurbents305@gmail.com>
2025-03-19 22:37:34 +0800

    更新 WebParser.kt

--- app/src/main/java/com/example/lanzou/network/WebParser.kt
+++ app/src/main/java/com/example/lanzou/network/WebParser.kt
@@ -1,3 +1,12 @@
+package com.example.lanzou.network
+import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.useragent.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import org.jsoup.Jsoup
+import java.net.URLEncoder
 class WebParser {
     private val client = HttpClient()
 
