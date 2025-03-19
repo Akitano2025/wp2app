4d4aba7ecc987141062ef65dedeadd141cf9f4f8
Akitano2025 <aneurbents305@gmail.com>
2025-03-19 13:58:35 +0800

    更新 MainActivity.kt

--- app/src/main/java/com/example/lanzou/MainActivity.kt
+++ app/src/main/java/com/example/lanzou/MainActivity.kt
@@ -1,4 +1,6 @@
 import androidx.activity.ComponentActivity
+import android.os.Bundle
+import androidx.annotation.CallSuper
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
